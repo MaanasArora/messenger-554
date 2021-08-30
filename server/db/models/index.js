a0f895ab07c875ca497d3553633a0848982dce95
Maanas Arora <maanasarora23@gmail.com>
2021-08-30 08:11:53 -0400

    Add message recipient table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,18 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Recipient = require("./recipient");
 
 // associations
 
 User.hasMany(Conversation);
 Conversation.belongsToMany(User, { through: "member" });
 Message.belongsTo(Conversation);
-Message.belongsToMany(User, { through: "reader" });
+Message.belongsToMany(User, { through: Recipient });
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
